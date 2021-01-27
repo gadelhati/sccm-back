3d84896b1f4d94bfad929bf8e0aba78fcef61ef7
Everton <everton@fattoriaweb.com.br>
2021-01-27 18:46:28 -0200

    mostrar Id

--- src/main/java/br/com/fattoria/sccm/configuration/RepositoryRestConfig.java
+++ src/main/java/br/com/fattoria/sccm/configuration/RepositoryRestConfig.java
@@ -1,25 +1,41 @@
 package br.com.fattoria.sccm.configuration;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import javax.persistence.metamodel.Type;
 
 @Configuration
 public class RepositoryRestConfig implements RepositoryRestConfigurer {
 
-   @Bean
-   public Validator validator() {
-       return new LocalValidatorFactoryBean();
-   }
+	@Autowired
+	private EntityManager entityManager;
 
-   @Override
-   public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
-       validatingListener.addValidator("afterCreate", validator());
-       validatingListener.addValidator("beforeCreate", validator());
-       validatingListener.addValidator("afterSave", validator());
-       validatingListener.addValidator("beforeSave", validator());
-   }
+	@Bean
+	public Validator validator() {
+		return new LocalValidatorFactoryBean();
+	}
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(
+                entityManager.getMetamodel().getEntities().stream()
+                .map(Type::getJavaType)
+                .toArray(Class[]::new));
+	}
+
+	@Override
+	public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+		validatingListener.addValidator("afterCreate", validator());
+		validatingListener.addValidator("beforeCreate", validator());
+		validatingListener.addValidator("afterSave", validator());
+		validatingListener.addValidator("beforeSave", validator());
+	}
 }
