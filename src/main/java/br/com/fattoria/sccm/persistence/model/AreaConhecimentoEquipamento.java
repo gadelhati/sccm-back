b42d17196020f297f6d56960abed32aecbd7ed7d
Everton <everton@fattoriaweb.com.br>
2021-01-29 09:35:43 -0200

    Mapeamento

--- src/main/java/br/com/fattoria/sccm/persistence/model/AreaConhecimentoEquipamento.java
+++ src/main/java/br/com/fattoria/sccm/persistence/model/AreaConhecimentoEquipamento.java
@@ -4,29 +4,35 @@ import java.io.Serializable;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "area_conhecimento_equipamento")
 public class AreaConhecimentoEquipamento implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	public AreaConhecimentoEquipamento() {}
-
-	public AreaConhecimentoEquipamento(AreaConhecimentoEquipamentoPK pk) {
-		this.pk = pk;
-	}
 
 	@EmbeddedId
 	private AreaConhecimentoEquipamentoPK pk;
 
-	public AreaConhecimentoEquipamentoPK getPk() {
-		return pk;
-	}
-
-	public void setPk(AreaConhecimentoEquipamentoPK pk) {
-		this.pk = pk;
-	}
-
+	@ManyToOne
+	@JoinColumn(name = "fk_area_conhecimento", insertable = false, updatable = false)
+	private AreaConhecimento areaConhecimento;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_equipamento", insertable = false, updatable = false)
+	private Equipamento equipamento;
+	
+	
 }
--- src/main/java/br/com/fattoria/sccm/persistence/model/EquipamentoDados.java
+++ src/main/java/br/com/fattoria/sccm/persistence/model/EquipamentoDados.java
@@ -4,29 +4,35 @@ import java.io.Serializable;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "equipamento_dados")
 public class EquipamentoDados implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	public EquipamentoDados() {}
-
-	public EquipamentoDados(EquipamentoDadosPK equipamentoDadosPK) {
-		this.equipamentoDadosPK = equipamentoDadosPK;
-	}
 
 	@EmbeddedId
 	private EquipamentoDadosPK equipamentoDadosPK;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_equipamento", insertable = false, updatable = false)
+	private Equipamento equipamento;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_tipo_dados", insertable = false, updatable = false)
+	private TipoDado tipoDado;
 
-	public EquipamentoDadosPK getEquipamentoDadosPK() {
-		return equipamentoDadosPK;
-	}
-
-	public void setEquipamentoDadosPK(EquipamentoDadosPK equipamentoDadosPK) {
-		this.equipamentoDadosPK = equipamentoDadosPK;
-	}
 
 }
