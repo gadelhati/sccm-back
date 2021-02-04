d8690d88d5afce31c4fc78c7c244e12f7412d797
Everton <everton@fattoriaweb.com.br>
2021-02-04 11:51:49 -0200

    @id em tipo endereço

--- src/main/java/br/com/fattoria/sccm/persistence/model/TipoEndereco.java
+++ src/main/java/br/com/fattoria/sccm/persistence/model/TipoEndereco.java
@@ -3,7 +3,7 @@ package br.com.fattoria.sccm.persistence.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -21,6 +21,7 @@ public class TipoEndereco implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private Long id;
 	
 	private String descricao;
