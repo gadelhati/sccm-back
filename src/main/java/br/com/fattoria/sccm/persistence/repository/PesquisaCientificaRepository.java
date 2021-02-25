39fb939a1eaf434a3631eb754ad23dc27bb855c8
Everton <everton@fattoriaweb.com.br>
2021-02-25 19:31:35 -0300

    Ordenando por PC

--- src/main/java/br/com/fattoria/sccm/persistence/repository/PesquisaCientificaRepository.java
+++ src/main/java/br/com/fattoria/sccm/persistence/repository/PesquisaCientificaRepository.java
@@ -1,5 +1,10 @@
 package br.com.fattoria.sccm.persistence.repository;
 
+import java.util.Collection;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
@@ -9,5 +14,9 @@ import br.com.fattoria.sccm.persistence.model.PesquisaCientifica;
 @Repository
 @RepositoryRestResource(collectionResourceRel = "pesquisas_cientificas", path="pesquisas_cientificas", exported = false)
 public interface PesquisaCientificaRepository extends CrudRepository<PesquisaCientifica, Long> {
+	
+	Collection<PesquisaCientifica> findAll(Sort sort);
+	
+	Page<PesquisaCientifica> findAll(Pageable page);
 
 }
