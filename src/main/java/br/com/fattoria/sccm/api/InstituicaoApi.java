4478d36da7d605f189f3b192e8eb5228ff176502
Everton <everton@fattoriaweb.com.br>
2021-04-14 11:44:32 -0300

    Ajuste cadastro de endereco

--- src/main/java/br/com/fattoria/sccm/api/InstituicaoApi.java
+++ src/main/java/br/com/fattoria/sccm/api/InstituicaoApi.java
@@ -3,8 +3,6 @@ package br.com.fattoria.sccm.api;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-import org.springframework.util.ObjectUtils;
-
 import br.com.fattoria.sccm.persistence.model.Endereco;
 import br.com.fattoria.sccm.persistence.model.Instituicao;
 import br.com.fattoria.sccm.persistence.model.TipoInstituicao;
@@ -41,22 +39,28 @@ public class InstituicaoApi {
 	
 	private String site;
 	
-	private String logradouro;
+//	private String logradouro;
+//	
+//	private String numero;
+//	
+//	private String complemento;
+//	
+//	private String cidade;
+//	
+//	private String cep;
 	
-	private String numero;
+	private EnderecoApi endereco;
 	
-	private String complemento;
-	
-	private String cidade;
-	
-	private String cep;
-	
-	//private EnderecoApi endereco;
+//	public Instituicao toEntity() {
+//		Endereco endereco = !ObjectUtils.isEmpty(logradouro) && !ObjectUtils.isEmpty(cidade) && !ObjectUtils.isEmpty(cep) && !ObjectUtils.isEmpty(numero) ? 
+//				new Endereco(id, null, logradouro, numero, complemento, cidade, cep) : null;
+//		return new Instituicao(id, nome, sigla, tipoInstituicao, numeroInscricao, numeroControle, null, ativo, email, site, endereco);//endereco != null ? endereco.toEntity() : null
+//	}
 	
 	public Instituicao toEntity() {
-		Endereco endereco = !ObjectUtils.isEmpty(logradouro) && !ObjectUtils.isEmpty(cidade) && !ObjectUtils.isEmpty(cep) && !ObjectUtils.isEmpty(numero) ? 
-				new Endereco(id, null, logradouro, numero, complemento, cidade, cep) : null;
-		return new Instituicao(id, nome, sigla, tipoInstituicao, numeroInscricao, numeroControle, null, ativo, email, site, endereco);//endereco != null ? endereco.toEntity() : null
+		Endereco enderecoEntity = endereco != null ? 
+				new Endereco(endereco.getId(), null, endereco.getLogradouro(), endereco.getNumero(), endereco.getComplemento(), endereco.getCidade(), endereco.getCep()) : null;
+		return new Instituicao(id, nome, sigla, tipoInstituicao, numeroInscricao, numeroControle, null, ativo, email, site, enderecoEntity);//endereco != null ? endereco.toEntity() : null
 	}
 
 }
