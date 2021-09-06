ad566a4d4554cd1e7501b9a22fd21505433f0dbc
Victor <victor@fattoriaweb.com.br>
2021-09-06 14:59:28 -0300

    Relatorio da Marinha

--- src/main/java/br/com/fattoria/sccm/controller/RelatorioController.java
+++ src/main/java/br/com/fattoria/sccm/controller/RelatorioController.java
@@ -1,8 +1,14 @@
 package br.com.fattoria.sccm.controller;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -13,7 +19,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fattoria.sccm.api.Periodo;
+import br.com.fattoria.sccm.dto.PendenciaDTO;
+import br.com.fattoria.sccm.persistence.model.PesquisaCientifica;
 import br.com.fattoria.sccm.persistence.repository.RelatorioRepository;
+import br.com.fattoria.sccm.reports.ReportFichaPesquisaCientifica;
 import br.com.fattoria.sccm.reports.ReportRelatorio;
 import br.com.fattoria.sccm.reports.data.RelatoriosDTO;
 import io.swagger.annotations.Api;
@@ -48,7 +57,20 @@ public class RelatorioController {
 		
 		dto.setPorSituacao(relatorioRepository.countByDataCadastroBetweenGroupBySituacao(periodo));
 		
-		dto.setPesquisaRecebidasPendentes(relatorioRepository.findByDataCadastroIdSituacaoBetween(periodo, 4L));
+		Collection<PesquisaCientifica> pcs = relatorioRepository.findByDataCadastroIdSituacaoBetween(periodo, 4L);
+		Collection<PendenciaDTO> listaPendencia = new LinkedList<PendenciaDTO>();
+		
+		if (pcs != null && !pcs.isEmpty()) {
+			
+			for (PesquisaCientifica pesquisaCientifica : pcs) {
+				
+				PendenciaDTO pendencia = new PendenciaDTO(pesquisaCientifica, periodo);		
+				listaPendencia.add(pendencia);
+			}
+			
+		}
+						
+		dto.setPesquisaRecebidasPendentes(listaPendencia);
 	
 		dto.setShipModeloPorSituacoes(relatorioRepository.sumModelosObservacoesMeteorologicasByDataCadastroBetweenGroupBySituacao(periodo, "ship"));
 		
@@ -58,7 +80,7 @@ public class RelatorioController {
 		
 		dto.setSynopInformacoesPorSituacoes(relatorioRepository.sumInformacaoObservacoesMeteorologicasByDataCadastroBetweenGroupBySituacao(periodo, "synop"));
 		
-		dto.setDadosEstacoesMeteorologicas(relatorioRepository.listagemDadosEstacoesMeteorologicasByDataCadastroBetweenGroupBySituacao(periodo));
+		/*dto.setDadosEstacoesMeteorologicas(relatorioRepository.listagemDadosEstacoesMeteorologicasByDataCadastroBetweenGroupBySituacao(periodo));*/
 		
 		dto.setMidiasParticularesPorSituacoes(relatorioRepository.countMidiasParticularesByDataCadastroBetweenGroupBySituacao(periodo));
 		
@@ -66,7 +88,42 @@ public class RelatorioController {
 		
 		
 		ReportRelatorio report = new ReportRelatorio(dto);
+		
+		InputStream in = RelatoriosDTO.class.
+				getResourceAsStream("/br/com/fattoria/sccm/reports/img/logo-chm.png");
 
+		report.addParametro("IMG_LOGO", in);
+		
+		report.addJasperSubReport("PARAM_TIPOS_COMISSAO", RelatoriosDTO.class
+				.getResourceAsStream("/br/com/fattoria/sccm/reports/templates/subreportTiposComissoes.jasper"));
+		
+		report.addJasperSubReport("PARAM_DADOS_RECEBIDOS", RelatoriosDTO.class
+				.getResourceAsStream("/br/com/fattoria/sccm/reports/templates/subreportDadosRecebidos.jasper"));
+		
+		report.addJasperSubReport("PARAM_SITUACAO", RelatoriosDTO.class
+				.getResourceAsStream("/br/com/fattoria/sccm/reports/templates/subreportSituacao.jasper"));
+		
+		report.addJasperSubReport("PARAM_PENDENCIA", RelatoriosDTO.class
+				.getResourceAsStream("/br/com/fattoria/sccm/reports/templates/subreportPendencia.jasper"));
+		
+		report.addJasperSubReport("PARAM_MOD_SHIP", RelatoriosDTO.class
+				.getResourceAsStream("/br/com/fattoria/sccm/reports/templates/subreportQTDModeloSHIP.jasper"));
+		
+		report.addJasperSubReport("PARAM_INF_SHIP", RelatoriosDTO.class
+				.getResourceAsStream("/br/com/fattoria/sccm/reports/templates/subreportQTDInfSHIP.jasper"));
+		
+		report.addJasperSubReport("PARAM_MOD_SYNOP", RelatoriosDTO.class
+				.getResourceAsStream("/br/com/fattoria/sccm/reports/templates/subreportQTDModelosSYNOP.jasper"));
+		
+		report.addJasperSubReport("PARAM_INF_SYNOP", RelatoriosDTO.class
+				.getResourceAsStream("/br/com/fattoria/sccm/reports/templates/subreportQTDInfSYNOP.jasper"));
+		
+		report.addJasperSubReport("PARAM_EMP_PARTICULAR", RelatoriosDTO.class
+				.getResourceAsStream("/br/com/fattoria/sccm/reports/templates/subreportQTDInfSYNOP.jasper"));
+		
+		report.addJasperSubReport("PARAM_DIVERSOS", RelatoriosDTO.class
+				.getResourceAsStream("/br/com/fattoria/sccm/reports/templates/subreportQTDInfSYNOP.jasper"));
+				
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		try {			
 			report.geraPDF(baos);
@@ -76,14 +133,14 @@ public class RelatorioController {
 
 		/*
 		report.addParametro("IMG_LOGO", ReportFichaPesquisaCientifica.class.
-				getResourceAsStream("/br/com/fattoria/sccm/reports/img/logo-chm.png"));
+				getResourceAsStream("/br/com/fattoria/sccm/reports/img/logo-chm.png"));*/
 		
 		byte[] arquivo = report.geraPDF();
 		
-		FileOutputStream fos = new FileOutputStream("E:\\Everton\\Área de Trabalho\\arq"+System.currentTimeMillis()+".pdf");
+		FileOutputStream fos = new FileOutputStream("C:\\Users\\Victor\\Desktop\\arq"+System.currentTimeMillis()+".pdf");
 		fos.write(arquivo);
 		fos.flush();
-		fos.close();*/
+		fos.close();
 		
 		//report.geraPDF("E:\\Everton\\Área de Trabalho\\teste"+System.currentTimeMillis()+".pdf");
 		
